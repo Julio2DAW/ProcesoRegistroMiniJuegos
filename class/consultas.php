c2be7f474fb0bc5804979829a9f8626f74b114dd
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-12-14 14:20:09 +0100

    archivos

---
+++ class/consultas.php
@@ -0,0 +1,18 @@
+<?php
+    require '../php/config.php'
+
+    class Consultas_SQL{
+        
+        function addLevel(){
+            $sql = 
+        }
+
+        function deleteLevel(){
+            $sql = 
+        }
+
+        function modifyLevel(){
+            
+        }
+    }
+?>
